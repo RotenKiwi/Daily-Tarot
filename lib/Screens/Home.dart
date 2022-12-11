5305551c7d6fff61d60a5287dc1956a80a5d76ee
RotenKiwi <joshiparth212@gmail.com>
2022-12-11 21:56:14 +0530

    added intl package
    implemented the correct package for date string function

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_intl/flutter_intl.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -7,7 +7,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    String date = DateTime.now().toString();
+    String date = DateFormat('dd-MMMM-yyyy').format(DateTime.now());
     return Container(
       decoration: const BoxDecoration(
         gradient: LinearGradient(
