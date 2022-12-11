78b5481508907eee8fcdd2ccd6c9d0bfeb70a27b
RotenKiwi <joshiparth212@gmail.com>
2022-12-11 20:10:19 +0530

    linear background
    added linear gradient background to home screen

---
+++ lib/Screens/Home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xff2c303c),
+            Color(0xff1B1F2E), //viridian : 6ca18b
+          ],
+        ),
+      ),
+    );
+  }
+}
